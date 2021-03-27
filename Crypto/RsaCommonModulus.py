5e811c9e938947215364395d5744039c244c5e24
Jen <j.hofbauer223@t-online.de>
2021-03-27 20:53:15 +0100

    Update RsaCommonModulus.py

--- Crypto/RsaCommonModulus.py
+++ Crypto/RsaCommonModulus.py
@@ -1,14 +1,15 @@
-from binascii import unhexlify
-from libnum import *
 from base64 import b64decode, b64encode
+from libnum import xgcd, invmod, n2s
 
 
-e1 = int('10001', 16)
-e2 = int('53cb7', 16)
+e1 = int('', 16)
+e2 = int('', 16)
 N = int('', 16)
-c1 = b64decode('')
-c2 = b64decode('')
+c1 = ''
+c2 = ''
 
+c1 = int.from_bytes(b64decode(c1), 'big')
+c2 = int.from_bytes(b64decode(c2), 'big')
 
 def common_modulus(e1, e2, c1, c2, N):
     # Extended Euclidean algorithm
@@ -27,4 +28,5 @@ def common_modulus(e1, e2, c1, c2, N):
     return [m, a, b, d]
 
 m, _, _, _ = common_modulus(e1,e2,c1,c2,N)
-print(m)
+flag = n2s(m)
+print(flag)
